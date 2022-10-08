dff0a4b951fdd279d5de2e3eec0bba89afa3f294
Andreas <111225446+iakmds@users.noreply.github.com>
2022-10-08 10:55:52 +0200

    Add necessary files for Spanish language

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -5,12 +5,15 @@ class Localization {
     const Locale('en', ''), //English first, as a fallback
 
     const Locale('de', ''),
+    const Locale('es', ''),
   ];
 
   static String getName(String code) {
     switch (code) {
       case 'de':
         return 'Deutsch';
+      case 'es':
+        return 'Español';
       case 'en':
       default:
         return 'English';
