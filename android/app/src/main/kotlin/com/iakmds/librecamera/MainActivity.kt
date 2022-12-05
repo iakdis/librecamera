563a0a48901d850951caf9a1fd3cece63ae57a63
iakmds <111225446+iakmds@users.noreply.github.com>
2022-12-05 16:06:31 +0100

    temp: Clean up and comment a Link for open gallery issue
    This thread talks about the same problem:
    https://stackoverflow.com/questions/34838866/how-to-scroll-among-images-viewed-via-intent-in-android

--- android/app/src/main/kotlin/com/iakmds/librecamera/MainActivity.kt
+++ android/app/src/main/kotlin/com/iakmds/librecamera/MainActivity.kt
@@ -1,95 +1,100 @@
 package com.iakmds.librecamera
 
-import android.content.ContentValues
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaActionSound
+import android.media.MediaScannerConnection
+import android.net.Uri
 import android.os.Build
-import android.os.Environment
-import android.provider.MediaStore
+import android.service.quicksettings.TileService
 import android.util.Log
-import android.webkit.MimeTypeMap
+import androidx.annotation.RequiresApi
+import androidx.core.content.FileProvider
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import java.io.File
-import java.io.IOException
-import android.media.MediaScannerConnection
-import android.content.Intent
-import android.net.Uri
-import androidx.core.content.FileProvider
-import android.content.pm.PackageManager
-import android.content.ComponentName
-import android.media.MediaActionSound
-import androidx.annotation.RequiresApi
-import android.service.quicksettings.TileService
-
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            "media_store"
-        ).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "updateItem" -> {
-                    updateItem(call.argument("path")!!)
-                    result.success(null)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "media_store")
+                .setMethodCallHandler { call, result ->
+                    when (call.method) {
+                        "updateItem" -> {
+                            updateItem(call.argument("path")!!)
+                            result.success(null)
+                        }
+                        "openItem" -> {
+                            openItem(
+                                    call.argument("path")!!,
+                                    call.argument("mimeType")!!,
+                                    call.argument("openInGallery")!!
+                            )
+                            result.success(null)
+                        }
+                        "disableIntentCamera" -> {
+                            disableIntentCamera(call.argument("disable")!!)
+                            result.success(null)
+                        }
+                        "shutterSound" -> {
+                            shutterSound()
+                            result.success(null)
+                        }
+                    }
                 }
-                "openItem" -> {
-                    openItem(call.argument("path")!!, call.argument("mimeType")!!, call.argument("openInGallery")!!)
-                    result.success(null)
-                }
-                "disableIntentCamera" -> {
-                    disableIntentCamera(call.argument("disable")!!)
-                    result.success(null)
-                }
-                "shutterSound" -> {
-                    shutterSound()
-                    result.success(null)
-                }
-            }
-        }
     }
 
     private fun updateItem(path: String) {
-        val file = File(path) 
-        MediaScannerConnection.scanFile(context, arrayOf(file.toString()),
-            null, null)
+        val file = File(path)
+        MediaScannerConnection.scanFile(context, arrayOf(file.toString()), null, null)
     }
 
     private fun openItem(path: String, mimeType: String, openInGallery: Boolean) {
         val file = File(path)
         var uri = FileProvider.getUriForFile(context, "com.iakmds.librecamera.provider", file)
-        if(openInGallery) {
+        if (openInGallery) {
             uri = Uri.parse("content:/$path")
         }
+
+        //https://stackoverflow.com/questions/34838866/how-to-scroll-among-images-viewed-via-intent-in-android
+        grantUriPermission(
+                "com.iakmds.librecamera.provider",
+                uri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+        )
 
         Intent().apply {
             action = Intent.ACTION_VIEW
             setDataAndType(uri, mimeType)
 
-            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            addFlags(
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            )
 
             startActivity(this)
         }
     }
 
-    private fun disableIntentCamera(disable: Boolean){
-        if(disable){
-            val pm = getApplicationContext().getPackageManager();
-            val compName = ComponentName(getPackageName(), "com.iakmds.librecamera.MainActivity");
+    private fun disableIntentCamera(disable: Boolean) {
+        if (disable) {
+            val pm = getApplicationContext().getPackageManager()
+            val compName = ComponentName(getPackageName(), "com.iakmds.librecamera.MainActivity")
             pm.setComponentEnabledSetting(
                     compName,
                     PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                    PackageManager.DONT_KILL_APP);
-        } else{
-            val pm = getApplicationContext().getPackageManager();
-            val compName = ComponentName(getPackageName(), "com.iakmds.librecamera.MainActivity");
+                    PackageManager.DONT_KILL_APP
+            )
+        } else {
+            val pm = getApplicationContext().getPackageManager()
+            val compName = ComponentName(getPackageName(), "com.iakmds.librecamera.MainActivity")
             pm.setComponentEnabledSetting(
                     compName,
                     PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                    PackageManager.DONT_KILL_APP);
+                    PackageManager.DONT_KILL_APP
+            )
         }
-        
     }
 
     private fun shutterSound() {
@@ -99,20 +104,17 @@ class MainActivity : FlutterActivity() {
 }
 
 @RequiresApi(Build.VERSION_CODES.N)
-class OpenTileService: TileService() {
+class OpenTileService : TileService() {
 
     override fun onClick() {
         super.onClick()
 
         try {
-            val newIntent = FlutterActivity
-                .withNewEngine()
-                .build(this)
+            val newIntent = FlutterActivity.withNewEngine().build(this)
             newIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             startActivityAndCollapse(newIntent)
-        }
-        catch (e:Exception){
-            Log.d("debug","Exception ${e.toString()}")
+        } catch (e: Exception) {
+            Log.d("debug", "Exception ${e.toString()}")
         }
     }
 }
