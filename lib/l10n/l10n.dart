e6ba8e655c4f9dd7595c86a0a87135236216cb53
iakmds <111225446+iakmds@users.noreply.github.com>
2023-01-09 19:49:51 +0100

    l10n: Update new languages

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -5,33 +5,48 @@ class Localization {
     const Locale('en', ''), //English first, as a fallback
 
     const Locale('de', ''),
+    const Locale('bg', ''),
     const Locale('es', ''),
     const Locale('eu', ''),
     const Locale('fr', ''),
+    const Locale('gl', ''),
     const Locale('ja', ''),
+    const Locale('nb', ''),
+    const Locale('pl', ''),
     const Locale('pt', ''),
     const Locale('ru', ''),
     const Locale('tr', ''),
+    const Locale('uk', ''),
   ];
 
   static String getName(String languageCode, {String countryCode = ''}) {
     switch (languageCode) {
       case 'de':
         return 'Deutsch';
+      case 'bg':
+        return 'Български';
       case 'es':
         return 'Español';
       case 'eu':
         return 'Euskara';
       case 'fr':
         return 'Français';
+      case 'gl':
+        return 'Galego';
       case 'ja':
         return '日本語';
+      case 'nb':
+        return 'Norsk bokmål';
+      case 'pl':
+        return 'Polski';
       case 'pt':
         return 'Português (do Brasil)';
       case 'ru':
         return 'Русский';
       case 'tr':
         return 'Türkçe';
+      case 'uk':
+        return 'Українська';
       case 'en':
       default:
         return 'English';
