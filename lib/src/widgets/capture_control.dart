816cd1a99ec87aff7de60d60302c52223de94efa
iakmds <111225446+iakmds@users.noreply.github.com>
2023-04-14 10:09:21 +0200

    feat(capture_control): Switch camera while recording video
    From Android 8.0

--- lib/src/widgets/capture_control.dart
+++ lib/src/widgets/capture_control.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:librecamera/main.dart';
@@ -40,6 +41,7 @@ class CaptureControlWidget extends StatefulWidget {
 class _CaptureControlWidgetState extends State<CaptureControlWidget>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
   @override
   void initState() {
@@ -57,140 +59,177 @@ class _CaptureControlWidgetState extends State<CaptureControlWidget>
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget pauseResumeButton() {
     final CameraController? cameraController = widget.controller;
 
+    return AnimatedRotation(
+      duration: const Duration(milliseconds: 400),
+      turns:
+          MediaQuery.of(context).orientation == Orientation.portrait ? 0 : 0.25,
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        onPressed: () => cameraController!.value.isRecordingPaused
+            ? widget.onResumeButtonPressed()
+            : widget.onPauseButtonPressed(),
+        icon: Stack(
+          alignment: Alignment.center,
+          children: [
+            const Icon(
+              Icons.circle,
+              color: Colors.black38,
+              size: 60,
+            ),
+            cameraController!.value.isRecordingPaused
+                ? const Icon(
+                    Icons.play_arrow,
+                    color: Colors.white,
+                    size: 30,
+                  )
+                : const Icon(
+                    Icons.pause,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+          ],
+        ),
+        tooltip: cameraController.value.isRecordingPaused
+            ? AppLocalizations.of(context)!.resumeVideo
+            : AppLocalizations.of(context)!.pauseVideo,
+        iconSize: 60,
+      ),
+    );
+  }
+
+  Widget captureButton() {
+    return AnimatedRotation(
+      duration: const Duration(milliseconds: 400),
+      turns:
+          MediaQuery.of(context).orientation == Orientation.portrait ? 0 : 0.25,
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        onPressed: widget.isVideoCameraSelected
+            ? () => widget.isRecordingInProgress
+                ? widget.onStopButtonPressed()
+                : widget.onVideoRecordButtonPressed()
+            : () => widget.onTakePictureButtonPressed(),
+        icon: Stack(
+          alignment: Alignment.center,
+          children: [
+            Icon(
+              Icons.circle,
+              color:
+                  widget.isVideoCameraSelected ? Colors.white : Colors.white38,
+              size: 80,
+            ),
+            Icon(
+              Icons.circle,
+              color: widget.isVideoCameraSelected ? Colors.red : Colors.white,
+              size: 65,
+            ),
+            Icon(
+              widget.isVideoCameraSelected && widget.isRecordingInProgress
+                  ? Icons.stop_rounded
+                  : Icons.videocam,
+              color: Colors.white,
+              size: 32,
+            ),
+            if (!widget.isVideoCameraSelected)
+              Icon(
+                Icons.camera_alt,
+                color: Colors.grey.shade800,
+                size: 32,
+              )
+          ],
+        ),
+        tooltip: widget.isVideoCameraSelected
+            ? widget.isVideoCameraSelected && widget.isRecordingInProgress
+                ? AppLocalizations.of(context)!.stopVideo
+                : AppLocalizations.of(context)!.startRecordingVideo
+            : AppLocalizations.of(context)!.takePicture,
+        iconSize: 80,
+      ),
+    );
+  }
+
+  Widget switchButton() {
+    return AnimatedRotation(
+      duration: const Duration(milliseconds: 400),
+      turns:
+          MediaQuery.of(context).orientation == Orientation.portrait ? 0 : 0.25,
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        onPressed: () {
+          widget.onNewCameraSelected(
+              cameras[widget.isRearCameraSelected ? 1 : 0]);
+          widget.setIsRearCameraSelected();
+
+          animationController.reset();
+          animationController.forward();
+        },
+        icon: Stack(
+          alignment: Alignment.center,
+          children: [
+            const Icon(
+              Icons.circle,
+              color: Colors.black38,
+              size: 60,
+            ),
+            AnimatedBuilder(
+              animation: animationController,
+              builder: (context, child) {
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.001)
+                    ..rotateY(animationController.value * 6),
+                  child: child,
+                );
+              },
+              child: Icon(
+                widget.isRearCameraSelected
+                    ? Icons.camera_front
+                    : Icons.camera_rear,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ],
+        ),
+        tooltip: widget.isRearCameraSelected
+            ? AppLocalizations.of(context)!.flipToFrontCamera
+            : AppLocalizations.of(context)!.flipToRearCamera,
+        iconSize: 60,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        /*CameraTogglesWidget(
-            controller: widget.controller,
-            onNewCameraSelected: widget.onNewCameraSelected,
-          ),*/
-        widget.leadingWidget,
-        AnimatedRotation(
-          duration: const Duration(milliseconds: 400),
-          turns: MediaQuery.of(context).orientation == Orientation.portrait
-              ? 0
-              : 0.25,
-          child: IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: widget.isVideoCameraSelected
-                ? () => widget.isRecordingInProgress
-                    ? widget.onStopButtonPressed()
-                    : widget.onVideoRecordButtonPressed()
-                : () => widget.onTakePictureButtonPressed(),
-            icon: Stack(
-              alignment: Alignment.center,
-              children: [
-                Icon(
-                  Icons.circle,
-                  color: widget.isVideoCameraSelected
-                      ? Colors.white
-                      : Colors.white38,
-                  //size: 80,
-                  size: 80,
-                ),
-                Icon(
-                  Icons.circle,
-                  color:
-                      widget.isVideoCameraSelected ? Colors.red : Colors.white,
-                  //size: 65,
-                  size: 65,
-                ),
-                Icon(
-                  widget.isVideoCameraSelected && widget.isRecordingInProgress
-                      ? Icons.stop_rounded
-                      : Icons.videocam,
-                  color: Colors.white,
-                  size: 32,
-                ),
-                if (!widget.isVideoCameraSelected)
-                  Icon(
-                    Icons.camera_alt,
-                    color: Colors.grey.shade800,
-                    size: 32,
-                  )
-              ],
-            ),
-            tooltip: widget.isVideoCameraSelected
-                ? widget.isVideoCameraSelected && widget.isRecordingInProgress
-                    ? AppLocalizations.of(context)!.stopVideo
-                    : AppLocalizations.of(context)!.startRecordingVideo
-                : AppLocalizations.of(context)!.takePicture,
-            iconSize: 80,
-          ),
-        ),
-        AnimatedRotation(
-          duration: const Duration(milliseconds: 400),
-          turns: MediaQuery.of(context).orientation == Orientation.portrait
-              ? 0
-              : 0.25,
-          child: IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: widget.isRecordingInProgress
-                ? () => cameraController!.value.isRecordingPaused
-                    ? widget.onResumeButtonPressed()
-                    : widget.onPauseButtonPressed()
-                : () {
-                    widget.onNewCameraSelected(
-                        cameras[widget.isRearCameraSelected ? 1 : 0]);
-                    widget.setIsRearCameraSelected();
+        widget.isRecordingInProgress
+            ? pauseResumeButton()
+            : widget.leadingWidget,
+        captureButton(),
+        FutureBuilder(
+          future: deviceInfo.androidInfo,
+          builder: (context, snapshot) {
+            if (!widget.isRecordingInProgress) return switchButton();
 
-                    animationController.reset();
-                    animationController.forward();
-                  },
-            icon: Stack(
-              alignment: Alignment.center,
-              children: [
-                const Icon(
-                  Icons.circle,
-                  color: Colors.black38,
-                  size: 60,
-                ),
-                widget.isRecordingInProgress
-                    ? cameraController!.value.isRecordingPaused
-                        ? const Icon(
-                            Icons.play_arrow,
-                            color: Colors.white,
-                            size: 30,
-                          )
-                        : const Icon(
-                            Icons.pause,
-                            color: Colors.white,
-                            size: 30,
-                          )
-                    : AnimatedBuilder(
-                        animation: animationController,
-                        builder: (context, child) {
-                          return Transform(
-                            alignment: Alignment.center,
-                            transform: Matrix4.identity()
-                              ..setEntry(3, 2, 0.001)
-                              ..rotateY(animationController.value * 6),
-                            child: child,
-                          );
-                        },
-                        child: Icon(
-                          widget.isRearCameraSelected
-                              ? Icons.camera_front
-                              : Icons.camera_rear,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                      ),
-              ],
-            ),
-            tooltip: widget.isRecordingInProgress
-                ? cameraController!.value.isRecordingPaused
-                    ? AppLocalizations.of(context)!.resumeVideo
-                    : AppLocalizations.of(context)!.pauseVideo
-                : widget.isRearCameraSelected
-                    ? AppLocalizations.of(context)!.flipToFrontCamera
-                    : AppLocalizations.of(context)!.flipToRearCamera,
-            iconSize: 60,
-          ),
+            if (snapshot.hasData) {
+              AndroidDeviceInfo androidInfo =
+                  snapshot.data as AndroidDeviceInfo;
+
+              if (androidInfo.version.sdkInt >= 26) {
+                return switchButton();
+              } else {
+                return const SizedBox(height: 60, width: 60);
+              }
+            } else {
+              return const SizedBox(height: 60, width: 60);
+            }
+          },
         ),
       ],
     );
