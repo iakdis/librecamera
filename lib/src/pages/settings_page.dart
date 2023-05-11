b8e96d2c9df99c89caf1881bce944ca085fb1a03
iakmds <111225446+iakmds@users.noreply.github.com>
2023-05-11 09:59:24 +0200

    bump: Version to 1.8.0

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -119,7 +119,7 @@ class _SettingsPageState extends State<SettingsPage> {
       icon: const Icon(Icons.info),
       applicationName: 'Libre Camera',
       applicationVersion: AppLocalizations.of(context)!.version(
-          '1.7.0'), //TODO change versions HERE AND pubspec.yaml at 'version'
+          '1.8.0'), //TODO change versions HERE AND pubspec.yaml at 'version'
       applicationIcon: const Image(
         image: AssetImage('assets/images/icon.png'),
         width: 50,
